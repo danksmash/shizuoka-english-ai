2d8a0312b3e66ac64e65bf4897ff226d54e838bb
danksmash <danksmash@gmail.com>
2026-09-02 07:12:20 +0900

    feat: map target 20 personas to unified WebP assets

--- src/data/studentImages.ts
+++ src/data/studentImages.ts
@@ -1,54 +1,57 @@
-import oliverImg from '../assets/images/oliver_uk.jpg';
-import emmaImg from '../assets/images/emma_usa.jpg';
-import liamImg from '../assets/images/liam_aus.jpg';
-import chloeImg from '../assets/images/chloe_can.jpg';
-import benceImg from '../assets/images/bence_hun.jpg';
-import zofiaImg from '../assets/images/zofia_pol.jpg';
-import rahulImg from '../assets/images/rahul_ban.jpg';
-import linhImg from '../assets/images/linh_vie.jpg';
-import aungImg from '../assets/images/aung_mya.jpg';
+import emmaImg from '../assets/personas/emma_usa.webp';
+import oliverImg from '../assets/personas/oliver_uk.webp';
+import liamImg from '../assets/personas/liam_australia.webp';
 import minjiImg from '../assets/personas/minji_korea.webp';
 import pavelImg from '../assets/personas/pavel_belarus.webp';
 import lukasImg from '../assets/personas/lukas_germany.webp';
 import ainaImg from '../assets/personas/aina_malaysia.webp';
 import dimasImg from '../assets/personas/dimas_indonesia.webp';
+import benceImg from '../assets/personas/bence_hungary.webp';
 import yutingImg from '../assets/personas/yuting_taiwan.webp';
+import zofiaImg from '../assets/personas/zofia_poland.webp';
 import matasImg from '../assets/personas/matas_lithuania.webp';
 import ananyaImg from '../assets/personas/ananya_india.webp';
 import xinyiImg from '../assets/personas/xinyi_china.webp';
+import linhImg from '../assets/personas/linh_vietnam.webp';
+import rahulImg from '../assets/personas/rahul_bangladesh.webp';
 import nadeeshaImg from '../assets/personas/nadeesha_srilanka.webp';
 import sumanImg from '../assets/personas/suman_nepal.webp';
 import amaraImg from '../assets/personas/amara_nigeria.webp';
 import andreiImg from '../assets/personas/andrei_romania.webp';
 
+// Non-target legacy personas retained for backward compatibility only.
+import chloeImg from '../assets/images/chloe_can.jpg';
+import aungImg from '../assets/images/aung_mya.jpg';
+
 export const STUDENT_AVATAR_MAP: Record<string, string> = {
-  oliver_uk: oliverImg,
   emma_usa: emmaImg,
+  oliver_uk: oliverImg,
   liam_aus: liamImg,
   liam_australia: liamImg,
-  chloe_can: chloeImg,
-  chloe_canada: chloeImg,
-  bence_hun: benceImg,
-  bence_hungary: benceImg,
-  zofia_pol: zofiaImg,
-  zofia_poland: zofiaImg,
-  rahul_ban: rahulImg,
-  rahul_bangladesh: rahulImg,
-  linh_vie: linhImg,
-  linh_vietnam: linhImg,
-  aung_mya: aungImg,
-  aung_myanmar: aungImg,
   minji_korea: minjiImg,
   pavel_belarus: pavelImg,
   lukas_germany: lukasImg,
   aina_malaysia: ainaImg,
   dimas_indonesia: dimasImg,
+  bence_hun: benceImg,
+  bence_hungary: benceImg,
   yuting_taiwan: yutingImg,
+  zofia_pol: zofiaImg,
+  zofia_poland: zofiaImg,
   matas_lithuania: matasImg,
   ananya_india: ananyaImg,
   xinyi_china: xinyiImg,
+  linh_vie: linhImg,
+  linh_vietnam: linhImg,
+  rahul_ban: rahulImg,
+  rahul_bangladesh: rahulImg,
   nadeesha_srilanka: nadeeshaImg,
   suman_nepal: sumanImg,
   amara_nigeria: amaraImg,
   andrei_romania: andreiImg,
+
+  chloe_can: chloeImg,
+  chloe_canada: chloeImg,
+  aung_mya: aungImg,
+  aung_myanmar: aungImg,
 };
