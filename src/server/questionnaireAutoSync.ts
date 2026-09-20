fe1a26afedd889204fd8fa2d387f347c5285a75a
danksmash <danksmash@gmail.com>
2026-09-21 08:58:35 +0900

    Import questionnaire scoring version in auto-sync

--- src/server/questionnaireAutoSync.ts
+++ src/server/questionnaireAutoSync.ts
@@ -5,6 +5,7 @@ import { STUDY1_FORMAL_PARTICIPANT_HASHES } from './study1FormalParticipantHashe
 import {
   QUESTIONNAIRE_COLLECTION,
   QUESTIONNAIRE_INSTRUMENT_VERSION,
+  QUESTIONNAIRE_SCORING_VERSION,
   QUESTIONNAIRE_ITEMS,
   calculateQuestionnaireScores,
   scoreQuestionnaireResponse,
